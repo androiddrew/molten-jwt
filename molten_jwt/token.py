269b5b5bdfe59d7cf4c5e43b4de43e5dba2427f8
androiddrew <bednar.andrew@gmail.com>
2018-09-22 15:13:12 -0400

    Added doc strings

--- molten_jwt/token.py
+++ molten_jwt/token.py
@@ -14,7 +14,11 @@ from .utils import get_token_from_header
 logger = logging.getLogger(__name__)
 
 
+# TODO add dynamic attribute access to the token contents
 class JWTUser:
+    """A `JWTUser` instance represents a decoded user token. All
+    token claims are stored within the `JTWUser.token` dictionary."""
+
     __slots__ = ("id", "user_name", "token")
 
     def __init__(self, id: Union[int, str], user_name: str, token: Dict) -> None:
@@ -24,6 +28,10 @@ class JWTUser:
 
 
 class JWT:
+    """The `JWT` instance is used to both encode and decode JSON Web Tokens
+    (JWTs) within your application. This class requires at a minimum that you
+    provide a `JWT_SECRET` within a `molten.Settings` dictionary"""
+
     def __init__(self, settings: Settings) -> None:
         self.secret: str = settings.get("JWT_SECRET")
         self.algorithm: str = settings.get("JWT_ALGORITHM", "HS256")
@@ -40,16 +48,19 @@ class JWT:
             )
 
     def encode(self, payload: Dict) -> str:
-        """Generates a JWT Auth token"""
+        """Generates a JWT auth token"""
         try:
 
-            return pyjwt.encode(payload, self.secret, algorithm=self.algorithm).decode(encoding="utf8")
+            return pyjwt.encode(payload, self.secret, algorithm=self.algorithm).decode(
+                encoding="utf8"
+            )
 
         except PyJWTError as err:
             return err
 
     # TODO Consider supporting multiple algorithms in decode
     def decode(self, token: str) -> Optional[Dict]:
+        """Decodes a JWT auth token"""
         try:
             payload = pyjwt.decode(
                 token, self.secret, algorithms=[self.algorithm], **self.options
@@ -71,6 +82,8 @@ class JWT:
         return payload
 
     def jwt_user_factory(self, token: str) -> JWTUser:
+        """A factory function for the creation of `molten_jwt.JWTUser
+        objects."""
         _token = self.decode(token)
         user_id = _token.get(self.identity_claim)
         user_name = _token.get(self.user_name_claim)
@@ -78,6 +91,20 @@ class JWT:
 
 
 class JWTComponent:
+    """A component that sets up a JWT instance for use in encoding
+    and decoding JSON Web Tokens. This component depends on the
+    availability of a `molten.Settings` component.
+
+    Your settings dictionary must contain a `JWT_SECRET` setting
+    at a minimum, for use in signing and verifying JWTs. Additionally,
+    you may include:
+
+    `JWT_ALGORITHM`: a string for the `PyJWT` supported alogrithm.
+     Defaults to `HS256`.
+
+    `JWT_AUTHORIZATION_PREFIX`: a string for the toke scheme. Defaults
+    to `bearer`."""
+
     is_cacheable = True
     is_singleton = True
 
@@ -89,6 +116,19 @@ class JWTComponent:
 
 
 class JWTUserComponent:
+    """A component that instantiates a JWTUser. This component
+    depends on the availability of a `molten.Settings`
+    component and on a `molten_jwt.JWT` component.
+
+    In addition to the `molten_jwt.JWT` configuration settings,
+    you can provide:
+
+    `JWT_USER_ID`: a string value for the claim representing
+    the user id within your token. Defaults to `sub`.
+
+    `JWT_USER_NAME`: a string value for the claim representing
+    the user name within your token. Defaults to `name`."""
+
     is_cacheable = True
     is_singleton = False
 
@@ -105,12 +145,16 @@ class JWTUserComponent:
 
 
 class JWTMiddleware:
-    """A middleware that automatically validates"""
+    """A middleware that automatically validates a JWT passed within
+    the `Authorization` header of the request. This middleware depends
+    on the availability of a `molten.Settings`component, a
+    `molten_jwt.JWT` component, and a molten_jwt.JWTUser` component.
+
+    Use the `molten_jwt.decoractors.allallow_anonymous` decorator to allow,
+    for non-authenticated access to endpoints when using this middleware"""
 
     def __call__(self, handler: Callable[..., Any]) -> Callable[..., Any]:
-        def middleware(
-            dependency_resolver: DependencyResolver, jwt_user: JWTUser
-        ) -> Any:
+        def middleware(jwt_user: JWTUser) -> Any:
             if getattr(handler, "allow_anonymous", False):
                 return handler()
 
@@ -124,4 +168,3 @@ class JWTMiddleware:
             return handler()
 
         return middleware
-
