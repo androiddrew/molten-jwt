e06847af2084c5da8a4135aea72f6822cc419476
androiddrew <bednar.andrew@gmail.com>
2019-01-27 19:36:38 -0500

    Bump version: 0.1.1 → 0.2.0

--- molten_jwt/__init__.py
+++ molten_jwt/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Drew Bednar"""
 __email__ = "drew@androiddrew.com"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 from molten_jwt.token import JWT, JWTComponent
 from molten_jwt.auth import JWTAuthMiddleware, JWTIdentity, JWTIdentityComponent
